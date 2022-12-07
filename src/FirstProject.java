7572398088a45f032c6108b3ac5e97a4e56498f6
Kozma Akos <kozmaakoss@gmail.com>
2022-12-07 11:07:06 +0200

    First Project

--- src/FirstProject.java
+++ src/FirstProject.java
@@ -2,7 +2,7 @@ public class FirstProject {
     public static void main(String[] args) {
 
         for (int i = 0; i <4 ; i++) {
-            System.out.println("Hello Bello");
+            System.out.println("Hello Bello ");
         }
 
 
